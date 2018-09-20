478602138272381cfca0669b6d035fc0aa5e29ae
liyilinlinlin <1337261972@qq.com>
2018-09-20 17:53:46 +0800

    three

--- src/components/search.vue
+++ src/components/search.vue
@@ -9,13 +9,29 @@
             <button class="search-btn" @click="search()">百度一下</button>
             <!-- 显示下拉数据 -->
             <div class="search-select">
-
+                <transition-group name="itenfade" tag="ul">
+                    <li class="select-option select-list" :class="{selectback:index==now}" v-for="(value,index) in myData" :key="index">
+                        {{value}}
+                    </li>
+                </transition-group>
             </div>
         </div>
     </div>
 </template>
 <script>
-    
+    export default{
+        data(){
+            return {
+                myData: [],
+            }
+        },
+        methods:{
+            // 清除文本框
+            clearInput:function(){
+
+            }
+        }
+    }
 </script>
 <style lang='stylus' scoped>
     .mysearch
@@ -50,4 +66,33 @@
                 font-size 16px
                 color #ffffff
                 font-weight bold
+            .search-select
+                position absolute
+                top 45px
+                width 500px
+                box-sizing border-box
+                ul 
+                    list-style none 
+                    text-align left 
+                    margin 0
+                    padding 0
+                    border-bottom 1px solid #d4d4d4
+                .itemfade-enter-active,.itemfade-leave-active
+                    transition all -5s
+                .itemfade-enter-active,.itemfade-leave-active
+                    opacity 0
+                li 
+                    border 1px solid #d4d4d4
+                    background #ffffff
+                    border-top none 
+                    border-bottom none 
+                    width 100%
+                .select-option
+                    box-sizing border-box
+                    padding 7px 10px
+                .select-list
+                    transition all .5s
+                .selectback
+                    background #eeeeee
+                    cursor pointer
 </style>
